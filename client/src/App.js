4787197e70d2c747cb2484525286518b1a7244d3
sergiodm92 <crgiodm@gmail.com>
2023-01-03 22:25:42 -0300

    asd

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 
+import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from "./components/LandingPage/LandingPage";
@@ -7,23 +8,14 @@ import Details from './components/Details/Details';
 import Home from './components/Home/Home';
 
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-  
-    
-      <Route exact path='/' component={LandingPage} />
-      <Route exact path='/home' component={Home} />
-      <Route exact path='/recipe' component={Form} />
-      <Route exact path='/recipes/:id' component={Details} />
-      
-   
-
-      
+        <Route exact path='/' component={LandingPage} />
+        <Route exact path='/home' component={Home} />
+        <Route exact path='/recipe' component={Form} />
+        <Route exact path='/recipes/:id' component={Details} />
     </div>
   );
 }
-
-export default App;
-
 
