489aaf4d3bf83eace58416ee071c7e4264e0b5ef
sergiodm92 <crgiodm@gmail.com>
2023-01-03 22:07:36 -0300

    build

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux'
 import store from "./store/index.js";
@@ -20,4 +18,4 @@ ReactDOM.render(
 );
 
 
-reportWebVitals();
+
